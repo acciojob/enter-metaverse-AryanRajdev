6e145ab32a9bff43a24bed94939ae3d3af9f0a97
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-20 16:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //your JS code here. If required.
-let parentDiv = document.querySelector("#parent");
-let mybtn = document.querySelector("#enterBtn");
+// let parentDiv = document.querySelector("#parent");
+// let mybtn = document.querySelector("#enterBtn");
+// let mystatus = document.querySelector("#status");
+const p = document.getElementById("status");
+const mybtn = document.getElementById("enterBtn");
 
-mybtn.addEventListener("click",function() {
-	parentDiv.innerHTML = "<h1>Entered Metaverse</h1>";
+mybtn.addEventListener("click",()=>{
+	p.innerHTML = "<h1>Entered Metaverse</h1>"
 });
