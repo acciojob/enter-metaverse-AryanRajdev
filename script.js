ba9ba25cb7fba4a968003c43d0c2007aad72e977
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-20 15:09:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let parentDiv = document.querySelector("#parent");
+let mybtn = document.querySelector("#enterBtn");
+
+mybtn.addEventListener("click",function() {
+	parentDiv.innerHTML = "<h1>Entered Metaverse</h1>";
+});
